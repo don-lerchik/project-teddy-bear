6d3d14cef8ad165084889c1762eb754bdd9bbdb0
Valery <don777lerchik@gmail.com>
2018-07-16 17:59:47 +0300

    Ends_catalog

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ var navSearchLink = document.querySelector(".main-nav__link--search");
 var navSearchNoclick = document.querySelector(".search__input");
 var modal = document.querySelector(".modal");
 var overlay = document.querySelector(".overlay");
-var modalBtn = document.querySelector(".btn--to-basket");
+var modalBtn = document.querySelectorAll(".btn--to-basket");
+var modalIcon = document.querySelectorAll(".product__to-basket")
 var modalClose = document.querySelector(".modal__close")
 navMain.classList.remove("main-nav--nojs");
 navMain.classList.add("main-nav--closed");
@@ -21,7 +22,7 @@ window.addEventListener("click", function () {
         navSearch.classList.add("main-nav__item--search--opend");
         return;
     }
-    if ((target == navToggle) || (target.parentNode == navToggle) ){
+    if ((target == navToggle) || (target.parentNode == navToggle)) {
         this.event.preventDefault();
         if (navMain.classList.contains("main-nav--closed")) {
             navMain.classList.remove("main-nav--closed");
@@ -32,18 +33,38 @@ window.addEventListener("click", function () {
         }
         return;
     }
-    if ((target == modalBtn) || (target.parentNode == modalBtn)) {
+    if (modalBtn) {
+        for (var key in modalBtn) {
+            if ((target == modalBtn[key]) || (target.parentNode == modalBtn[key])) {
+                this.event.preventDefault();
+                modal.classList.add("modal--show");
+                overlay.classList.add("overlay--show");
+                return;
+            }
+        }
+    }
+    if (modalIcon) {
+        for (var key in modalIcon) {
+            if ((target == modalIcon[key]) || (target.parentNode == modalIcon[key])) {
+                this.event.preventDefault();
+                modal.classList.add("modal--show");
+                overlay.classList.add("overlay--show");
+                return;
+            }
+        }
+    }
+ /*    if (((target == modalBtn) || (target.parentNode == modalBtn)) || ((target == modalIcon) || (target.parentNode == modalIcon))) {
         this.event.preventDefault();
         modal.classList.add("modal--show");
         overlay.classList.add("overlay--show");
         return;
-    }
+    } */
     if ((target == modalClose) || (target.parentNode == modalClose)) {
         this.event.preventDefault();
         modal.classList.remove("modal--show");
         overlay.classList.remove("overlay--show");
         return;
-    }
+    } 
     if (target != navSearchNoclick) {
         navSearch.classList.remove("main-nav__item--search--opend");
         navSearch.classList.add("main-nav__item--search--closed");
